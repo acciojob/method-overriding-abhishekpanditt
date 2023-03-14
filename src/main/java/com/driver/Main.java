4913b6948327108a1887ac75917b16eb480af01a
abhishekpanditt <badi.abhi@gmail.com>
2023-03-14 17:00:49 +0530

    Project created

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,28 @@
 package com.driver;
 
 public class Main {
+
+    public static void main(String[] args) {
+        B b = new B();
+
+        System.out.println(b.meth());
+
+        B b1 = new B();
+
+        System.out.println(b1.meth());
+
+    }
+
+    public static class A{
+        public String meth(){
+            return "Invoking method from class A";
+        }
+    }
+
+    public static class B extends A{
+        public String meth(){
+            return "Method is overridden in Extendend class B";
+        }
+    }
   
 }
